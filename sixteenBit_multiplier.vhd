6425b0c29f1fca38d6053487deb46fc6c23e8753
CodyDPittman <94379219+CodyDPittman@users.noreply.github.com>
2023-10-22 18:15:31 -0400

    Fixed the overflow not updating!

--- sixteenBit_multiplier.vhd
+++ sixteenBit_multiplier.vhd
@@ -15,19 +15,19 @@ architecture structural of sixteenBit_multiplier is
 signal check1, check2 : signed(31 downto 0);
 
 begin	
-	process
-	begin  
+	
 	product <= A * B;
 	
 	check1 <= A * B;
 	check2 <= A * B;
 	
+	process(check1, check2)
+	begin
 	-- Check if product is > 16 bits (32,767) or (-32,768)
-	if check1 > 32767 then
+	if to_integer(check1) > 32767 then
 		overflow <= '1'; 
-	elsif check2 < -32768 then
+	elsif to_integer(check2) < -32768 then
 		overflow <= '1';
 	end if;	 
-	wait;
 	end process;
 end structural;
