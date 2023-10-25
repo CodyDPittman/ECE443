9a472eeced70cd0939bd4466d868309b44c1f911
CodyDPittman <94379219+CodyDPittman@users.noreply.github.com>
2023-10-25 11:15:13 -0400

    Added some comments for clarity.

--- sixteenBit_multiplexer.vhd
+++ sixteenBit_multiplexer.vhd
@@ -6,22 +6,24 @@ use ieee.numeric_std.all;
 
 entity sixteenBit_multiplexer is
 	port(sel : in std_logic_vector(2 downto 0); -- select lines
-			I0, I1, I2, I3, I4, I5, I6, I7 : signed (15 downto 0); -- inputs
+			I0, I1, I2, I3, I4, I5, I6, I7 : in signed (15 downto 0); -- inputs
 			output : out signed(15 downto 0)); --output
 end entity;
 
 
 architecture structural of sixteenBit_multiplexer is
+
 begin
 	
+	
 with sel select
-	output <= I0 when "000",
-			  I1 when "001",
-			  I2 when "010",
-			  I3 when "011",
-			  I4 when "100",
-			  I5 when "101",
-			  I6 when "110",
-			  I7 when "111",
+	output <= I0 when "000",  -- signed addition
+			  I1 when "001",  -- signed multiplication
+			  I2 when "010",  -- passthrough A
+			  I3 when "011",  -- passthrough B
+			  I4 when "100",  -- signed subtraction
+			  I5 when "101",  -- dont care
+			  I6 when "110",  -- dont care
+			  I7 when "111",  -- dont care
 			  "0000000000000000" when others;
 end structural;
