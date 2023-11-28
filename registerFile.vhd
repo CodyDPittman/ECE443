d2db7290f84d6f78e5027a0b8bdae8d6da00ac9c
CodyDPittman <94379219+CodyDPittman@users.noreply.github.com>
2023-11-28 16:20:06 -0500

    Add files via upload

--- registerFile.vhd
+++ registerFile.vhd
@@ -1,35 +1,51 @@
 library IEEE;
 use IEEE.std_logic_1164.all;
-use ieee.numeric_std.all;
+use ieee.numeric_std.all;	
 
 --- VHDL code for a 8x16 register file ---	 
---- 8 registers each 16 bits in length
-	
+--- 8 registers each 16 bits in length 
+
+package RF is
+	type register_File is array(0 to 7) of signed (15 downto 0);
+end package RF;
+
+use work.RF.all;
+
+library IEEE;
+use IEEE.std_logic_1164.all;
+use ieee.numeric_std.all;	
+
 entity registerFile is
 	port(	clk : in std_logic;	 -- clock
 			outA, outB : out signed (15 downto 0); 	-- read data of register A and B
 			input : in signed(15 downto 0);	  -- input from operation
-			regASel, regBSel : std_logic_vector(2 downto 0); -- selection of register to use
+			regASel, regBSel : in std_logic_vector(2 downto 0); -- selection of register to use
 			writeEnable : in std_logic;	  -- write enable to the register
-			writeRegSel : std_logic_vector(2 downto 0) -- selecting which register to write to
+			writeRegSel : in std_logic_vector(2 downto 0); -- selecting which register to write to
+			registers : inout register_File
 		);
 end registerFile;
 
 architecture behavioral of registerFile is
-	type register_File is array(0 to 7) of signed(15 downto 0);	-- Declaring type register_File	
-	signal registers : register_File;  -- Declaring array "registers" of type register_File
-		
-begin
-	Read: process(clk)	   -- Reading from the register file
-	begin
-		outA <= registers(to_integer(unsigned(regASel)));
-		outB <= registers(to_integer(unsigned(regBSel)));	
-	end process;
+begin 	
 	
-	Write: process(clk)	  -- Writing to the register file
+	process(clk)
 	begin
-		if(clk = '1' AND clk'event) then -- if clock is on the rising edge.....	
-		   if(writeEnable = '1') then  -- and if writeEnable = 1, write to the selected register
+		-- Initialize registers to 0
+	   registers(0) <= "0000000000000000";
+	   registers(1) <= "0000000000000011";
+	   registers(2) <= "0000000000011000";
+	   registers(3) <= "0000000000000000";
+	   registers(4) <= "0000000000000000";
+	   registers(5) <= "0000000000000000";
+	   registers(6) <= "0000000000000000";
+	   registers(7) <= "0000000000000000";
+	   
+	  	outA <= registers(to_integer(unsigned(regASel)));
+		outB <= registers(to_integer(unsigned(regBSel)));
+	
+		--if(clk = '1' AND clk'event) then -- if clock is on the rising edge.....	
+		   if(writeEnable = '1') then  -- and if writeEnable = 1, write to the selected register	   
 			 registers(to_integer(unsigned(writeRegSel))) <= input; -- write input to selected register
 			 if regASel = writeRegSel then	-- if Register A = register to write to
 				 outA <= input;
@@ -37,9 +53,9 @@ begin
 			 if regBSel = writeRegSel then -- if Register B = register to write to
 				 outB <= input;
 			 end if;   
-			end if;
+			--end if;
 		end if;
-	end process;
+	end process;		
 end behavioral;
 	
 		
--- registerfile_TB.vhd
+++ registerfile_TB.vhd
@@ -1,6 +1,10 @@
-library ieee;
-use ieee.NUMERIC_STD.all;
-use ieee.std_logic_1164.all;
+library IEEE;
+use IEEE.std_logic_1164.all;
+use ieee.numeric_std.all;	
+
+
+library registerFile;
+use registerFile.RF.all;
 
 	-- Add your library and packages declaration here ...
 
@@ -18,7 +22,8 @@ architecture TB_ARCHITECTURE of registerfile_tb is
 		regASel : in STD_LOGIC_VECTOR(2 downto 0);
 		regBSel : in STD_LOGIC_VECTOR(2 downto 0);
 		writeEnable : in STD_LOGIC;
-		writeRegSel : in STD_LOGIC_VECTOR(2 downto 0) );
+		writeRegSel : in STD_LOGIC_VECTOR(2 downto 0);
+		registers : inout register_File );
 	end component;
 
 	-- Stimulus signals - signals mapped to the input and inout ports of tested entity
@@ -26,20 +31,17 @@ architecture TB_ARCHITECTURE of registerfile_tb is
 	signal input : SIGNED(15 downto 0) := "0000000000000110";
 	signal regASel : STD_LOGIC_VECTOR(2 downto 0) := "001";
 	signal regBSel : STD_LOGIC_VECTOR(2 downto 0) := "010";
-	signal writeEnable : STD_LOGIC := '1';
-	signal writeRegSel : STD_LOGIC_VECTOR(2 downto 0) := "100";
-	-- Observed signals - signals mapped to the output ports of tested entity
-	signal outA : SIGNED(15 downto 0) := "0000000000000000";
-	signal outB : SIGNED(15 downto 0) := "0000000000000000";
-
-	-- Add your code here ...  
-    type register_File is array(0 to 7) of signed(15 downto 0);	
+	signal writeEnable : STD_LOGIC := '0';
+	signal writeRegSel : STD_LOGIC_VECTOR(2 downto 0) := "011";
 	signal registers : register_File;
-	signal temp1, temp2 : signed(15 downto 0);
-		
-begin 
-	
-	
+	-- Observed signals - signals mapped to the output ports of tested entity
+	signal outA : SIGNED(15 downto 0);
+	signal outB : SIGNED(15 downto 0);
+
+	-- Add your code here ...
+	signal tempA, tempB : signed(15 downto 0);
+begin
+
 	-- Unit Under Test port map
 	UUT : registerfile
 		port map (
@@ -50,37 +52,19 @@ begin
 			regASel => regASel,
 			regBSel => regBSel,
 			writeEnable => writeEnable,
-			writeRegSel => writeRegSel
+			writeRegSel => writeRegSel,
+			registers => registers
 		);
 
 	-- Add your stimulus here ... 
-	process(clk)
-	begin
-	registers(0) <= "0000000000000000";	 
-	registers(1) <= "0000000000000001";
-	registers(2) <= "0000000000000011";
-	registers(3) <= "0000000000000000";
-	registers(4) <= "0000000000000000";
-	registers(5) <= "0000000000000000";
-	registers(6) <= "0000000000000000";
-	registers(7) <= "0000000000000000";
-	
-	registers(to_integer(unsigned(writeRegSel))) <= input;
-	--registers(to_integer(unsigned(regASel))) => outA;
-	--registers(to_integer(unsigned(regBSel))) => outB;
-	end process;  
+    	
 	
 		
-	
-	
-	temp1 <= registers(to_integer(unsigned(regASel))); -- temp1 is outA
-	temp2 <= registers(to_integer(unsigned(regBSel))); -- temp2 is outB
-	
-	
-	
-	--registerFile1: registerfile port map (clk => clk, temp1 => outA, temp2 => outB, input => input, regASel => regASel, regBSel => regBSel, writeEnable <= writeEnable, writeRegSel <= writeRegSel);
-	
-	
+		tempA <= registers(to_integer(unsigned(regASel)));
+		tempB <= registers(to_integer(unsigned(regBSel))); 	
+		
+	 
+	  
 end TB_ARCHITECTURE;
 
 configuration TESTBENCH_FOR_registerfile of registerfile_tb is
